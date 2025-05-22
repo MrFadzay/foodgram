cc695fb644051c75af257765a84a78eb6b259e4f
MrFadzay <janwise94@gmail.com>
2025-05-22 10:18:30 +0400

    fix urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/', include(('api.urls_v1', 'v1'), namespace='v1')),
-    # path('v2/', include('api.urls_v2')),  # Для будущих версий API
+    path('', include(('api.urls_v1', 'v1'), namespace='v1')),
 ]
