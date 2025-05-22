4137d600b04840b3c2a2d616328b439615f53085
MrFadzay <janwise94@gmail.com>
2025-05-22 09:32:11 +0400

    Upgrade React and React DOM to version 17.0.2, update related dependencies, and remove TypeScript from lock files.

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = () => {};
+}
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+}
