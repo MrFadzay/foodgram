8941ea492bee2ca132ba78acc5a4f54f53b79732
MrFadzay <janwise94@gmail.com>
2025-05-25 12:32:31 +0400

    change media root

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
 
 AUTH_USER_MODEL = 'users.User'
 
