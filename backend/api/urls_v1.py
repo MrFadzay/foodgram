d30eadbd716f4a8251d99247fdcf084105803fbf
MrFadzay <janwise94@gmail.com>
2025-06-07 01:51:37 +0400

    Refactor: Move recipe-related endpoints from recipes.urls to api.urls_v1 for better organization

--- backend/api/urls_v1.py
+++ backend/api/urls_v1.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from djoser import views as djoser_views
+from api.views import RecipeViewSet
 
 app_name = 'v1'
 
@@ -21,5 +22,27 @@ urlpatterns = [
     ),
     path('users/', include(('users.urls', 'users'), namespace='users')),
     path('', include(('recipes.urls', 'recipes'), namespace='recipes')),
+    path(
+        'recipes/<int:pk>/favorite/',
+        RecipeViewSet.as_view({'post': 'favorite', 'delete': 'favorite'}),
+        name='recipes-favorite'
+    ),
+    path(
+        'recipes/<int:pk>/shopping_cart/',
+        RecipeViewSet.as_view(
+            {'post': 'shopping_cart', 'delete': 'shopping_cart'}
+        ),
+        name='recipes-shopping-cart'
+    ),
+    path(
+        'recipes/download_shopping_cart/',
+        RecipeViewSet.as_view({'get': 'download_shopping_cart'}),
+        name='recipes-download-shopping-cart'
+    ),
+    path(
+        'recipes/<int:pk>/get-link/',
+        RecipeViewSet.as_view({'get': 'get_link'}),
+        name='recipes-get-link'
+    ),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -12,26 +12,4 @@ router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path(
-        'recipes/<int:pk>/favorite/',
-        RecipeViewSet.as_view({'post': 'favorite', 'delete': 'favorite'}),
-        name='recipes-favorite'
-    ),
-    path(
-        'recipes/<int:pk>/shopping_cart/',
-        RecipeViewSet.as_view(
-            {'post': 'shopping_cart', 'delete': 'shopping_cart'}
-        ),
-        name='recipes-shopping-cart'
-    ),
-    path(
-        'recipes/download_shopping_cart/',
-        RecipeViewSet.as_view({'get': 'download_shopping_cart'}),
-        name='recipes-download-shopping-cart'
-    ),
-    path(
-        'recipes/<int:pk>/get-link/',
-        RecipeViewSet.as_view({'get': 'get_link'}),
-        name='recipes-get-link'
-    ),
 ]
